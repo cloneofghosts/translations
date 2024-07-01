63d031a44aa82c0de8a0dab6e6b25c3e84ae13f0
Kev <cloneofghosts@users.noreply.github.com>
2024-07-01 15:28:19 -0400

    Add back eslint-plugin-promise

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
+import pluginPromise from 'eslint-plugin-promise'
 const nodePlugin = require("eslint-plugin-n");
 
 module.exports = [
   nodePlugin.configs["flat/recommended-script"],
+  pluginPromise.configs['flat/recommended'],
   {
     languageOptions: {
       ecmaVersion: 2024,
